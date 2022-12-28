c69347dfb5fa463f38e0ad4f0182df560a82c13c
Xiangyu Liu <neulxy@gmail.com>
2022-12-28 21:59:16 +1300

    create reverse pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ShortenPipe } from './shorten.pipe';
 import { FilterPipe } from './filter.pipe';
+import { RevsersePipe } from './revserse.pipe';
 
 @NgModule({
-  declarations: [AppComponent, ShortenPipe, FilterPipe],
+  declarations: [AppComponent, ShortenPipe, FilterPipe, RevsersePipe],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/revserse.pipe.ts
@@ -0,0 +1,10 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'revserse',
+})
+export class RevsersePipe implements PipeTransform {
+  transform(value: string): string {
+    return value.split('').reverse().join('');
+  }
+}
